7a94459cd1957858505aafd2221c4e9cb331359d
JakeHub852 <caseythomasjacob@gmail.com>
2025-05-26 19:16:09 +0100

    Added reduce hero health to code snippets

---
+++ code-snippets/2025-05-26_reduce-hero-health.py
@@ -0,0 +1,13 @@
+player_health = 1000
+
+player_health = player_health - 100  # reduce by 100 here
+print(player_health)
+
+player_health = player_health - 100  # and here
+print(player_health)
+
+player_health = player_health - 100  # and here
+print(player_health)
+
+player_health = player_health - 100  # and here
+print(player_health)
