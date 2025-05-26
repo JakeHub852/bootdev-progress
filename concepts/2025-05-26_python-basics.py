ae942b024685b25b65507191c69ac17ed65630fc
JakeHub852 <caseythomasjacob@gmail.com>
2025-05-26 20:27:44 +0100

    Added Python basics to concepts

---
+++ concepts/2025-05-26_python-basics.py
@@ -0,0 +1,35 @@
+# 🐍 Python Basics  
+📅 Date: 2025-05-26
+
+## 📌 Variables
+
+A variable is just a name that stores a value.
+
+Example
+my_height = 100
+
+- Variable names should be descriptive.
+- Use a single token: lowercase with underscores (snake_case)
+- You can store numbers, text, or other data types.
+
+Mathematical Operators
+
+summation = a + b      # Addition
+difference = a - b     # Subtraction
+product = a * b        # Multiplication
+quotient = a / b       # Division
+
+Use parenthesis to control the order of operations:
+
+avg = (a + b + c) / 3
+
+Comments make code easier to understand but are ignored by Python
+
+# This is a comment
+
+For multi-line comments or documentation, use triple quotes:
+
+"""
+This is a multi-line comment
+or documentation string (docstring).
+"""
